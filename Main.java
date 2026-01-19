dc2b68e89284d9ff022508cee5cc607e124cb597
Kody Graham <kodydavison@hotmail.com>
2026-01-19 17:32:10 -0600

    Finally got GitHub lined out

---
+++ Main.java
@@ -0,0 +1,19 @@
+//Kody Graham
+//01/18/2026
+//Phase 1
+//For: Dr. Emre Celebi's Data Clustering Online Class - 4372
+
+//Coding practices resource I have decided to primarily use: https://www.cs.cornell.edu/courses/JavaAndDS/JavaStyle.html
+
+class UniformRandomSelection {
+    public static void main()
+    {
+        check
+    }
+}
+
+
+
+//Java Practices and Style Guide from my Google search: This one seems to cover a little more https://google.github.io/styleguide/javaguide.html
+//However, I like the layout of the sections better on this one, so I will reference both (they should contain basically the same information as these practice are kind of industry standard I think)
+//but I will primarily use https://www.cs.cornell.edu/courses/JavaAndDS/JavaStyle.html
